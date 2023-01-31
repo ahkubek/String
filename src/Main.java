227faa867a543e3ab9172cabc86bbfcb270e84a4
ahkubek <120653318+ahkubek@users.noreply.github.com>
2023-01-31 09:58:50 +0300

    String

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        //task1
+        System.out.println();
+        System.out.println("ZADACHA1");
+        String firstName = "Ivan";
+        String middleName    = "Ivanich";
+        String lastName  = "Ivanov";
+        String fullName   = "Ivanov Ivan Ivanovich";
+        System.out.println("ФИО СОТРУДНИКА"+ fullName);
+        //TASK2
+        System.out.println();
+        
+        System.out.println("ZADACHA2");
+        String small = "Ivanov Ivan Ivanovich";
+        System.out.println(small.toUpperCase());
+
+        fullName = "Семён Семёнович";
+        //Task3
+        System.out.println();
+        System.out.println("Задача3");
+
+
+    }
+}
